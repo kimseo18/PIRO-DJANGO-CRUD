1792468993001045bcd4a5f2533951051cfb977d
kimseoyeong <kimseoyeong108@gmail.com>
2023-01-12 22:55:31 +0900

    type hint 추가

--- server/apps/posts/views.py
+++ server/apps/posts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
+from django.http.request import HttpRequest
+
 from server.apps.posts.models import Post
 
 # Create your views here.
 def hello_world(request, *args, **kwargs):
     return render(request, "posts/hello_world.html")
 
-def posts_list(request, *args, **kwargs):
+def posts_list(request:HttpRequest, *args, **kwargs):
     text=request.GET.get("text")
     posts=Post.objects.all()
 
@@ -14,12 +16,12 @@ def posts_list(request, *args, **kwargs):
 
     return render(request, "posts/posts_list.html",{"posts":posts})
 
-def posts_retrieve(request, pk, *args, **kwargs):
+def posts_retrieve(request:HttpRequest, pk, *args, **kwargs):
     post=Post.objects.all().get(id=pk)
     return render(request, "posts/posts_retrieve.html", {"post":post})
 
 
-def posts_create(request, *args, **kwargs):
+def posts_create(request:HttpRequest, *args, **kwargs):
     if request.method == "POST":
         Post.objects.create(
             title=request.POST["title"],
@@ -32,7 +34,7 @@ def posts_create(request, *args, **kwargs):
         return redirect("/")
     return render(request, "posts/posts_create.html")
 
-def posts_update(request, pk, *args, **kwagrs):
+def posts_update(request:HttpRequest, pk, *args, **kwagrs):
     post=Post.objects.get(id=pk)
 
     if request.method=="POST":
@@ -46,7 +48,7 @@ def posts_update(request, pk, *args, **kwagrs):
 
     return render(request, "posts/posts_update.html",{"post":post})
 
-def posts_delete(request, pk, *args, **kwargs):
+def posts_delete(request:HttpRequest, pk, *args, **kwargs):
     if request.method=="POST":
         post=Post.objects.get(id=pk)
         post.delete()
